7a0ded657147e4e2ebd71490e02ab759fa79190d
Waki Abdurrazak <waki4d@gmail.com>
2021-12-10 09:53:22 +0700

    commit ulang tanpa master data

--- src/main/java/com/darkotech/app/config/CacheConfiguration.java
+++ src/main/java/com/darkotech/app/config/CacheConfiguration.java
@@ -48,20 +48,6 @@ public class CacheConfiguration {
             createCache(cm, com.darkotech.app.domain.User.class.getName());
             createCache(cm, com.darkotech.app.domain.Authority.class.getName());
             createCache(cm, com.darkotech.app.domain.User.class.getName() + ".authorities");
-            createCache(cm, com.darkotech.app.domain.Absence.class.getName());
-            createCache(cm, com.darkotech.app.domain.Region.class.getName());
-            createCache(cm, com.darkotech.app.domain.Country.class.getName());
-            createCache(cm, com.darkotech.app.domain.Location.class.getName());
-            createCache(cm, com.darkotech.app.domain.Department.class.getName());
-            createCache(cm, com.darkotech.app.domain.Department.class.getName() + ".employees");
-            createCache(cm, com.darkotech.app.domain.Task.class.getName());
-            createCache(cm, com.darkotech.app.domain.Task.class.getName() + ".jobs");
-            createCache(cm, com.darkotech.app.domain.Employee.class.getName());
-            createCache(cm, com.darkotech.app.domain.Employee.class.getName() + ".absences");
-            createCache(cm, com.darkotech.app.domain.Employee.class.getName() + ".jobs");
-            createCache(cm, com.darkotech.app.domain.Job.class.getName());
-            createCache(cm, com.darkotech.app.domain.Job.class.getName() + ".tasks");
-            createCache(cm, com.darkotech.app.domain.JobHistory.class.getName());
             // jhipster-needle-ehcache-add-entry
         };
     }
--- src/main/webapp/app/shared/model/country.model.ts
+++
@@ -1,9 +0,0 @@
-import { IRegion } from 'app/shared/model/region.model';
-
-export interface ICountry {
-  id?: number;
-  countryName?: string | null;
-  region?: IRegion | null;
-}
-
-export const defaultValue: Readonly<ICountry> = {};
